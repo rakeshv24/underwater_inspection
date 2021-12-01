ddca1a2beb93030c320cfb6032d28c7653bdec11
Rakesh Vivekanandan <rakeshvivek97@gmail.com>
2021-11-30 16:03:39 -0800

    added map_carving

---
+++ src/map_carving.cpp
@@ -0,0 +1,229 @@
+#include "../include/underwater_inspection/map_carving.h"
+#include <tf/transform_datatypes.h>
+
+#include <string>
+
+namespace map_carving_ns {
+
+MapCarving::MapCarving() {
+}
+
+bool MapCarving::initialize() {
+
+  ROS_INFO("[map_carving] Node launched");
+  if (!readParameters()) {
+    ROS_ERROR("readParameters failed!");
+    return false;
+  }
+
+  ros::NodeHandle nh("");
+
+  // Initialize subscribers
+  map_sub_ = nh.subscribe(map_topic_, 1, &MapCarving::collisonMapCallback, this);
+  odometry_sub_ = nh.subscribe(odometry_topic_, 1, &MapCarving::odometryCallback, this);
+  viewpoint_sub_ = nh.subscribe(viewpoint_topic_, 1, &MapCarving::viewpointCallback, this);
+
+  // Initialize publishers
+  viewpoint_map_pub_ = nh.advertise<geometry_msgs::PoseArray>(viewpoint_map_topic_, 2);
+  
+  // Initialize tf listener
+  tf_listener_ = std::unique_ptr<tf::TransformListener>(new tf::TransformListener());
+
+  ROS_INFO("[map_carving][initialize] Successfully launched map_carving node");
+  std::cout << "[map_carving][initialize] Successfully launched map_carving node" << std::endl;
+  
+  return true;
+}
+
+bool MapCarving::readParameters() {
+
+  ros::NodeHandle nh("~");
+
+  if (!nh.getParam("viewpoint_map_topic_", viewpoint_map_topic_)) {
+    ROS_ERROR("Node: map_carving. Cannot read parameter: viewpoint_map_topic_");
+    return false;
+  }
+  if (!nh.getParam("odometry_topic_", odometry_topic_)) {
+    ROS_ERROR("Node: map_carving. Cannot read parameter: odometry_topic_");
+    return false;
+  }
+  if (!nh.getParam("viewpoint_topic_", viewpoint_topic_)) {
+    ROS_ERROR("Node: map_carving. Cannot read parameter: viewpoint_topic_");
+    return false;
+  }
+  if (!nh.getParam("map_topic_", map_topic_)) {
+    ROS_ERROR("Node: map_carving. Cannot read parameter: map_topic_");
+    return false;
+  }
+  if (!nh.getParam("ravenLength", ravenLength)) {
+    ROS_ERROR("Node: map_carving. Cannot read parameter: ravenLength");
+    return false;
+  }
+  if (!nh.getParam("ravenWidth", ravenWidth)) {
+    ROS_ERROR("Node: map_carving. Cannot read parameter: ravenWidth");
+    return false;
+  }
+  if (!nh.getParam("ravenHeight", ravenHeight)) {
+    ROS_ERROR("Node: map_carving. Cannot read parameter: ravenHeight");
+    return false;
+  }
+  if (!nh.getParam("ravenSphereRad", ravenSphereRad)) {
+    ROS_ERROR("Node: map_carving. Cannot read parameter: ravenSphereRad");
+    return false;
+  }
+  if (!nh.getParam("unknownThresh", unknownThresh)) {
+    ROS_ERROR("Node: map_carving. Cannot read parameter: unknownThresh");
+    return false;
+  }
+  if (!nh.getParam("freeThresh", freeThresh)) {
+    ROS_ERROR("Node: map_carving. Cannot read parameter: freeThresh");
+    return false;
+  }
+  if (!nh.getParam("occThresh", occThresh)) {
+    ROS_ERROR("Node: map_carving. Cannot read parameter: occThresh");
+    return false;
+  }
+  return true;
+}
+
+void MapCarving::viewpointCallback(const inspection_planner_msgs::Viewpoint::ConstPtr &viewpoint_msg) {
+  // ROS_INFO("[map_carving][viewpointCallback] viewpoint received");
+  // std::cout << "[map_carving][viewpointCallback] viewpoint received" << std::endl;
+  obtainViewpointInfo(viewpoint_msg->x, viewpoint_msg->y, viewpoint_msg->z, viewpoint_msg->yaw);
+}
+
+void MapCarving::collisonMapCallback(const octomap_msgs::Octomap::ConstPtr &map_msg) {
+  octomap::AbstractOcTree* abstract_tree_ = octomap_msgs::msgToMap(*map_msg);
+  // octomap::OcTree* octree = NULL;
+  // ROS_INFO("[map_carving][collisonMapCallback] Received Octomap");
+  // std::cout << "[map_carving][collisonMapCallback] Received Octomap" << std::endl;
+  
+  if (abstract_tree_){
+    // octree = dynamic_cast<octomap::OcTree*>(abstract_tree_);
+    tree_initialized_ = true;
+    collision_tree_ = dynamic_cast<octomap::OcTree*>(abstract_tree_);
+    std::size_t node_count_ = collision_tree_->calcNumNodes();
+    std::cout << "[map_carving][collisonMapCallback] Tree node count: " << node_count_ << std::endl;
+  }
+
+}
+
+void MapCarving::odometryCallback(const nav_msgs::Odometry &odom_msg) {
+  odom_ = odom_msg.pose.pose;
+  // ROS_INFO("[map_carving][odometryCallback] odom received");
+  // std::cout << "[map_carving][odometryCallback] odom received" << std::endl;
+  geo_quat_ = odom_.orientation;
+  tf::Matrix3x3(tf::Quaternion(geo_quat_.x, geo_quat_.y, geo_quat_.z, geo_quat_.w)).getRPY(raven_roll, raven_pitch, raven_yaw);
+}
+
+//////////////////////////////////
+// Publishers
+
+void MapCarving::publishViewpointInfo() {
+
+  ROS_INFO("[map_carving][publishViewpointInfo] publishing viewpoint map info");
+  std::cout << "[map_carving][publishViewpointInfo] publishing viewpoint map info" << std::endl;
+  
+}
+
+
+//////////////////////////////////
+// Main functions
+
+void MapCarving::obtainViewpointInfo(double vp_x, double vp_y, double vp_z, double vp_yaw){
+  ROS_INFO("[map_carving][obtainViewpointInfo] Map carving started.....");
+  std::cout << "[map_carving][obtainViewpointInfo] Map carving started....." << std::endl;
+
+  if(tree_initialized_){
+    double step_size = 0.25;
+
+    int unknown_cells_ = 0;
+    int free_cells_ = 0;
+    int occ_cells_ = 0;
+    double total_points = 0;
+    octomap::point3d ray_origin_pose (vp_x, vp_y, vp_z);
+    
+    double nod_fov = M_PI_2;
+    double nod_angle_inc = 0.05;
+    double h_fov = 130 * (M_PI/180);
+    // double v_fov = 20 * (M_PI/180);
+    // int ray_skip = 10;
+    double n_beams = 512;
+    double angle_inc = h_fov / n_beams;
+    
+    for(double pitch_direction = -nod_fov/2; pitch_direction <= nod_fov/2; pitch_direction += nod_angle_inc){
+      for(double yaw_direction = wrapAngle(vp_yaw-h_fov/2); yaw_direction <= wrapAngle(vp_yaw+h_fov/2); yaw_direction += angle_inc){
+        
+        double d_x = 1.0;
+        double d_y = 0.0;
+        double d_z = 0.0;
+
+        // Rotation about y-axis
+        d_x = d_x * std::cos(pitch_direction) - d_z * std::sin(pitch_direction);
+        d_z = d_x * std::sin(pitch_direction) + d_z * std::cos(pitch_direction);
+        
+        // Rotation about z-axis
+        d_x = d_x * std::cos(yaw_direction) - d_y * std::sin(yaw_direction);
+        d_y = d_y * std::sin(yaw_direction) + d_y * std::cos(yaw_direction);
+
+        // d_x += vp_x;
+        // d_y += vp_y;
+        // d_z += vp_z;
+
+        octomap::point3d ray_direction (d_x, d_y, d_z);
+        octomap::point3d ray_end_pt;
+        // std::cout<<ray_direction;
+        collision_tree_->setResolution(0.64);
+        bool ray_hit_success = collision_tree_->castRay(ray_origin_pose, ray_direction, ray_end_pt, true, 30.0);
+        // std::cout<<pitch_direction<<", "<<yaw_direction<<", "<<ray_origin_pose<<", "<<ray_direction<<", "<<ray_end_pt<<", "<<ray_hit_success<<std::endl;
+        if(ray_hit_success)
+          occ_cells_ += 1;
+      }
+    }
+    
+    
+    // double unknown_dist_ = (double)unknown_cells_ / (double)total_points;
+    // double free_dist_ = (double)free_cells_ / (double)total_points;
+    // double occ_dist_ = (double)occ_cells_ / (double)total_points;
+    // std::cout<<"[MapCarving][obtainViewpointInfo] Total Points: "<<total_points<<std::endl;
+    // std::cout<<"[MapCarving][obtainViewpointInfo] Unknown space: "<<unknown_dist_<<"; Number: "<<unknown_cells_<<std::endl;
+    std::cout<<"[MapCarving][obtainViewpointInfo] Occupied cells: "<<occ_cells_<<std::endl;
+    // std::cout<<"[MapCarving][obtainViewpointInfo] Free space: "<<free_dist_<<"; Number: "<<free_cells_<<std::endl;
+  }
+  else{
+    std::cout<<"[MapCarving][obtainViewpointInfo] Tree not initialized"<<std::endl;
+  }
+}
+
+//////////////////////////////////
+// Auxiliary functions
+
+double MapCarving::wrapAngle(double angle){
+  return std::fmod((angle + M_PI), (2 * M_PI)) - M_PI;
+}
+}
+
+int main(int argc, char** argv){
+
+  ROS_INFO("[map_carving][main] Creating node...");
+
+  ros::init(argc, argv, "map_carving");
+
+  map_carving_ns::MapCarving map_carving;
+
+  // Setup
+  bool result = map_carving.initialize();
+
+  ros::Duration(0.5).sleep(); // sleep added to flush log buffer (not working?)
+
+  // Loop indefinitely
+  if(result) {
+    ROS_INFO("[map_carving][main] Spinning indefinitely...");
+    ros::spin();
+  }
+
+  ROS_INFO("[map_carving][main] Exiting...");
+
+  return 0;
+}
+
